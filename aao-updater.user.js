be9385b44d5b7690293ef29ceef725c66f486859
Nino Rossi <43315787+NR1986@users.noreply.github.com>
2023-05-17 17:57:18 +0200

    Update aao-updater.user.js

--- aao-updater.user.js
+++ aao-updater.user.js
@@ -10,36 +10,68 @@
 
 (function() {
     'use strict';
+
+    // URL zur externen Textdatei
     var textFileURL = "https://aao-update.nirolp.de/version.txt";
-    var button = document.createElement("button");
-    button.innerHTML = "Button-Text";
-    button.style.marginLeft = "10px";
-    button.style.backgroundColor = "#3e8f3e";
+    
+    // Der aktuelle Text des Buttons
+    var currentButtonText = "";
 
-    GM_xmlhttpRequest({
-        method: "GET",
-        url: textFileURL,
-        onload: function(response) {button.innerHTML = response.responseText;}
-    });
+    // Die Funktion zum Erstellen des Buttons und Laden des Textes
+    function createButton() {
+        // Den Button erstellen und formatieren
+        var button = document.createElement("button");
+        button.innerHTML = currentButtonText || "Button-Text";
+        button.style.marginLeft = "10px";
+        button.style.backgroundColor = "#3e8f3e";
 
-    function openLinkInNewWindow(event) {event.preventDefault();window.open("https://aao-update.nirolp.de", "_blank");}
-
-    function updateButtonText() {button.innerHTML = "Update";}
-
-    function resetButtonText() {
+        // Die Textdatei laden
         GM_xmlhttpRequest({
             method: "GET",
             url: textFileURL,
-            onload: function(response) {button.innerHTML = response.responseText;}
+            onload: function(response) {
+                var newText = response.responseText.trim();
+                // Überprüfen, ob der Text der Textdatei geändert wurde
+                if (newText !== currentButtonText) {
+                    currentButtonText = newText;
+                    button.innerHTML = currentButtonText;
+                }
+            }
         });
+
+        // Funktion zum Öffnen des Links in einem neuen Fenster
+        function openLinkInNewWindow(event) {
+            event.preventDefault();
+            window.open("https://aao-update.nirolp.de", "_blank");
+        }
+
+        // Funktion zum Ändern des Button-Textes bei Mousehover
+        function updateButtonText() {
+            button.innerHTML = "Update";
+        }
+
+        // Funktion zum Zurücksetzen des Button-Textes
+        function resetButtonText() {
+            button.innerHTML = currentButtonText;
+        }
+
+        // Den Button mit der Funktion und dem Link versehen
+        button.addEventListener("click", openLinkInNewWindow);
+        button.addEventListener("mouseover", updateButtonText);
+        button.addEventListener("mouseout", resetButtonText);
+
+        // Den Navbar-Bereich auswählen und den Button einfügen
+        var navbar = document.querySelector(".nav.navbar-nav.navbar-right");
+        var listItem = document.createElement("li");
+        listItem.appendChild(button);
+        navbar.appendChild(listItem);
     }
 
-    button.addEventListener("click", openLinkInNewWindow);
-    button.addEventListener("mouseover", updateButtonText);
-    button.addEventListener("mouseout", resetButtonText);
+    // Die Update-Abfrage beim Ausführen des Skripts starten
+    createButton();
 
-    var navbar = document.querySelector(".nav.navbar-nav.navbar-right");
-    var listItem = document.createElement("li");
-    listItem.appendChild(button);
-    navbar.appendChild(listItem);
+    // Interval zum Überprüfen auf Aktualisierungen
+    setInterval(function() {
+        createButton();
+    }, 60000); // Alle 60 Sekunden überprüfen
 })();
